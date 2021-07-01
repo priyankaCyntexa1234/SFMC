893952613a1cfb2a055e155c03918f625f9de817
ANIL KUMAR <63443121+AndoAnil@users.noreply.github.com>
2021-07-01 22:43:53 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -99,22 +99,14 @@ define([
             case 'step3':
                 $('#step3').show();
                 connection.trigger('updateButton', {
-                     button: 'back',
-                     visible: true
+                    button: 'back',
+                    visible: true
                 });
-                if (lastStepEnabled) {
-                    connection.trigger('updateButton', {
-                        button: 'next',
-                        text: 'next',
-                        visible: true
-                    });
-                } else {
-                    connection.trigger('updateButton', {
-                        button: 'next',
-                        text: 'done',
-                        visible: true
-                    });
-                }
+                connection.trigger('updateButton', {
+                    button: 'next',
+                    text: 'next',
+                    visible: true
+                });
                 break;
             case 'step4':
                 $('#step4').show();
